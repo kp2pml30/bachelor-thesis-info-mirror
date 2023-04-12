8a34aac2108ec350fe73cabf4db6767d985e096b
K Prokopenko <prokopenko.kirill1@huawei-partners.com>
2023-04-12 09:44:40 +0300

    improve scripts

--- save-artifacts.sh
+++ save-artifacts.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 set -e
+set -x
 
 D="$(mktemp -d)"
 
 mv "./artifacts" "$D"
 
+function undo {
+	mv "$D/artifacts" "."
+	rm -rf "$D"
+}
+
 CB="$(git branch --show-current)"
-git checkout artifacts
+git checkout artifacts || (undo && exit 1)
 rm -rf "./artifacts"
 cp -r "$D/artifacts" "./artifacts"
 git add artifacts
 git commit --amend --no-edit
 git checkout "$CB"
 
-mv "$D" "./artifacts"
-
-rm -rf "$D"
+undo
